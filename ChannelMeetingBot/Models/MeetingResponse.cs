74fb77f6bcd8f37a71599d135df1a445743c4c63
Felipe Dotti <felipedotti@microsoft.com>
2023-03-15 22:35:27 -0300

    Commiting to master

---
+++ ChannelMeetingBot/Models/MeetingResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeetingChannelBot.Models
+{
+    public class MeetingResponse
+    {
+        public System.Net.HttpStatusCode StatusCode { get; set; }
+        public string MeetingLink { get; set; }
+        public DateTimeOffset? StartDateTime { get; set; }
+        public DateTimeOffset? EndDateTime { get; set; }
+    }
+}
